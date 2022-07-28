4c57a5223230e45a6eed94ef69b748a458ffc3fb
WatchJani <jankokondic84@gmail.com>
2022-07-28 21:41:41 +0200

    update link

--- controllers/textEditorController.js
+++ controllers/textEditorController.js
@@ -3,6 +3,6 @@ const AppError = require('../utils/appError');
 
 
 module.exports.upload_Post = catchAsync(async (req, res, next) => {
-    const image = req.file?.path;
-    res.status(200).json(image)
-  });
+  const image = req.file?.path;
+  res.status(200).json(image)
+});
